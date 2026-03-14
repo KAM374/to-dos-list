893a29fea33b25100901bf35eda12d1ffca319e3
KAM374 <kerri.marshall12@gmail.com>
2026-03-13 21:08:36 -0400

    Update app.py
    indentation correction

--- app.py
+++ app.py
@@ -4,24 +4,22 @@ app = Flask(__name__)
 def todo_list():
     return "To-Do List"
  while True:
- 
- tasks = []
-
-  i=0
-  i=i+1
-  action = input("Type 'add', 'delete', 'quit': ")
-
-  if action == "add":
-    task = input("enter a task: ")
-    tasks.append(task)
+     tasks = []
+      i=0
+      i=i+1
+      action = input("Type 'add', 'delete', 'quit': ")
     
-  elif action == "delete":
-    num = int(input("enter the task number to delete: "))
-    tasks.pop(num-1)
-    print("Your To-Do List \n",  tasks)
-    
-  elif  action == "quit":
-    print("Your To-Do List \n",  tasks)
+      if action == "add":
+        task = input("enter a task: ")
+        tasks.append(task)
+        
+      elif action == "delete":
+        num = int(input("enter the task number to delete: "))
+        tasks.pop(num-1)
+        print("Your To-Do List \n",  tasks)
+        
+      elif  action == "quit":
+        print("Your To-Do List \n",  tasks)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)
